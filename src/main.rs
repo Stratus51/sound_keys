19cf02c986bec3dbd878958cffcc5821328746b3
Igor Valet <igor_valet@hotmail.com>
2020-10-09 23:50:41 +0200

    Translate keyboard events

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,41 @@ const EMPTY_EVENT: input_linux::sys::input_event = input_linux::sys::input_event
     value: 0,
 };
 
-fn watch_device(
-    device_path: &str,
-    mut output: tokio::sync::mpsc::Sender<input_linux::sys::input_event>,
-) {
+#[derive(Debug, Clone, Copy)]
+enum KeyState {
+    Up,
+    Down,
+    Repeat,
+    Unknown,
+}
+
+impl KeyState {
+    fn from_i32(value: i32) -> Self {
+        match value {
+            0 => Self::Up,
+            1 => Self::Down,
+            2 => Self::Repeat,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct KeyEvent {
+    sec: i64,
+    usec: i64,
+    code: u16,
+    value: KeyState,
+}
+
+const EV_KEY: u16 = input_linux::sys::EV_KEY as u16;
+
+fn watch_device(device_path: &str, mut output: tokio::sync::mpsc::Sender<KeyEvent>) {
     let device_path = device_path.to_string();
     std::thread::spawn(move || {
         println!("Opening file {}", device_path);
         let fd =
-            std::fs::File::open(device_path).expect("Designated paths should be valid devices");
+            std::fs::File::open(&device_path).expect("Designated paths should be valid devices");
         let input = input_linux::evdev::EvdevHandle::new(fd);
         loop {
             let mut events: [input_linux::sys::input_event; 1] = [EMPTY_EVENT; 1];
@@ -31,16 +57,24 @@ fn watch_device(
                 }
             };
             for ev in events.iter().take(n) {
-                while let Err(e) = output.try_send(*ev) {
-                    eprintln!("Error sending event to main thread: {:?}", e);
-                    std::thread::sleep(std::time::Duration::from_millis(100));
+                if ev.type_ == EV_KEY {
+                    let ev = KeyEvent {
+                        sec: ev.time.tv_sec,
+                        usec: ev.time.tv_usec,
+                        code: ev.code,
+                        value: KeyState::from_i32(ev.value),
+                    };
+                    while let Err(e) = output.try_send(ev) {
+                        eprintln!("Error sending event to main thread: {:?}", e);
+                        std::thread::sleep(std::time::Duration::from_millis(100));
+                    }
                 }
             }
         }
     });
 }
 
-async fn handle_events(mut input: tokio::sync::mpsc::Receiver<input_linux::sys::input_event>) {
+async fn handle_events(mut input: tokio::sync::mpsc::Receiver<KeyEvent>) {
     while let Some(event) = input.recv().await {
         println!("Received event {:?}", event);
     }
